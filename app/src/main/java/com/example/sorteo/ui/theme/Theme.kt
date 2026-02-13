7548baaee707ceb4000bb9b5eb59f9983db844b4
jackxxs <jackxxx1305@gmail.com>
2026-02-14 00:56:05 +0100

    version 1.1 cambio de color

--- app/src/main/java/com/example/sorteo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/sorteo/ui/theme/Theme.kt
@@ -9,28 +9,29 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = Color(0xFF1C1B1F), // Fondo oscuro estándar
+    surface = Color(0xFF2C2B2F), // Superficie un poco más clara
+    onBackground = Color.White, // Texto sobre fondo oscuro
+    onSurface = Color.White // Texto sobre superficies
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    // COLORES PERSONALIZADOS
+    background = Color.White, // Fondo de la app en blanco
+    surface = Color(0xFFF7F7F7), // Un gris muy claro para las tarjetas
+    onBackground = Color.Black, // Texto sobre el fondo blanco
+    onSurface = Color.Black // Texto sobre las tarjetas
 )
 
 @Composable
