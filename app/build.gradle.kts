c463ea1a9e0a3bfed984a86adf44a91ecc3f3bc2
jackxxs <jackxxx1305@gmail.com>
2026-02-13 20:31:34 +0100

    version inicial del proyecto 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,13 @@ dependencies {
 
     // Guardado local
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+    
+    // Dependencias de Test
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
